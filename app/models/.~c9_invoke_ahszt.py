bbb948620478b7d110182d0588cb0b2a828037ff
EC2 Default User <ec2-user@ip-172-31-12-14.us-east-2.compute.internal>
2019-06-27 17:01:22 +0000

    my first commit

---
+++ app/models/.~c9_invoke_ahszt.py
@@ -0,0 +1,76 @@
+def FindTurtle(state):
+    if state == "Alabama":
+        return "Alabama Map Turtle, Box Turtle, Alabama Red-Bellied Cooter"
+    elif state == "Alaska":
+        return "Loggerhead Sea Turtle, Leatherback Sea Turtle, Green Sea Turtle"
+    elif state == "Arizona":
+        return "Snapping Turtle, Painted Turtle, Ornate Box Turtle, Pond Slider, Yellow Mud Turtle, Sonora Mud Turtle, Arizona Mud Turtle"
+    elif state == "Arkansas":
+        return "Snapping Turtle, Alligator Snapping Turtle, Southern Painted Turtle, Western Chicken Turtle, Northern Map Turtle, Ouachita Map Turtle, Mississippi Map Turtle, Eastern River Cooter, Three-toed Box Turtle, Ornate Box Turtle,Red-eared Slider, Mississippi Mud Turtle, Razor-backed Musk Turtle, Eastern Musk Turtle, Midland Smooth Softshell, Spiny Softshell, Yellow Mud Turtle, Western Painted Turtle"
+    elif state == 'California ':
+        return "Loggerhead Turtle, Olive Ridley Turtle, Leatherback Turtle"
+    elif state == 'Colorado':
+        return "Western Painted Turtle, Snapping Turtle, Yellow Mud Turtle, Spiny Softshell Turtle, Ornate Box Turtle"
+    elif state == 'Connecticut':
+        return "Atlantic Ridley Turtle or Kemp's Ridley Turtle, Green Turtle, Leatherback Turtle, Loggerhead Turtle, Bog turtle, Box turtle, Diamondback terrapin, Musk turtle, Painted turtle, Snapping turtle, Spotted turtle, Wood turtle"
+    elif state == "Delaware":
+        return "Common Musk Turtle (Stinkpot) Sternotherus odoratus, Common Snapping Turtle Chelydra serpentina serpentina, Eastern Box Turtle Terrapene carolina carolina, Eastern Mud Turtle Kinosternon subrubrum subrubrum, Eastern Painted Turtle Chrysemys picta picta, Northern Diamondback Terrapin Malaclemys terrapin terrapin, Northern Redbelly Turtle Pseudemys rubriventris, Red-eared Slider Trachemys scripta elegans, Spotted Turtle Clemmys guttata"
+    elif state == "Florida":
+        return  "Florida Softshell, Gulf Coast Spiny Softshell, Loggerhead Sea Turtle, Green Sea Turtle, Florida Snapping Turtle, Common Snapping Turtle, Spotted Turtle, Florida Chicken Turtle, Eastern Chicken Turtle, Leatherback Sea Turtle, Gopher Tortoise, Barbours Map Turtle, Striped Mud Turtle, Eastern Mud Turtle, Alligator Snapping Turtle, Carolina Diamondback Terrapin, Ornate Diamondback Terrapin, Mangrove Diamondback Terrapin, Eastern Florida Diamondback Terrapin, River Cooter, River Cooter, Florida Redbelly Coote, Peninsula Cooter, Suwannee Cooter, Loggerhead Musk Turtle, Stripeneck Musk Turtle, Eastern Musk Turtle, Florida Box Turtle, Eastern Box Turtle, Gulf Coast Box Turtle, Three-toed Box Turtle, Red-eared Slider, Yellowbelly Slider"
+    elif state == "Georgia":
+        return "Common Snapping Turtle (Chelydra serpentina), Alligator Snapping Turtle (Macrochelys temminckii) Painted Turtle (Chrysemys picta) Spotted Turtle (Clemmys guttata)Chicken Turtle (Deirochelys reticularia)Bog Turtle (Glyptemys muhlenbergii)Barbour’s Map Turtle (Graptemys barbouri)Alabama Map Turtle (Graptemys pulchra)Common Map Turtle (Graptemys geographica)Diamondback Terrapin (Malaclemys terrapin)River Cooter (Pseudemys concinna)Florida Cooter (Pseudemys floridana)Florida Redbelly Turtle (Pseudemys nelsoni)Box Turtle (Terrapene carolina)Slider Turtle (Trachemys scripta)Striped Mud Turtle (Kinosternon baurii)Eastern Mud Turtle (Kinosternon subrubrum)Stripeneck/Loggerhead Musk Turtle (Sternotherus minor)Common Musk Turtle (Sternotherus odoratus)Florida Softshell (Apalone ferox)Spiny Softshell (Apalone spinifera)Loggerhead Sea Turtle (Caretta caretta)Green Sea Turtle (Chelonia mydas)Hawksbill Sea Turtle (Eretmochleys imbricata)Kemps Ridley Sea Turtle (Lepidochelys kempii)Family DermochelyidaeLeatherback Sea Turtle (Dermochelys coriacea)"
+    elif state == "Hawaii":
+        return "Hawaiian Green Sea Turtles, Hawksbill Sea Turtles, Leatherback Turtles"
+    elif state == "Idaho":
+        return "The Painted Turtle is the only species of turtle that occurs naturally in Idaho so identifying them should be straight forward.  However, due to escaped pets and introduced species, one must become familiar with the general characteristics of Painted Turtles to avoid confusing them with such turtles as Box Turtles, and Red-eared Sliders (both common pet species)."
+    elif state == 'Illinois':
+        return "Painted Turtle, Spotted Turtle, Blandings Turtle, Nothern Map Turtle, Ouachita Map Turtle, False Map Turtle, Eastern River Cooter, Eastern Box Turtle, Ornate Box Turtle, Pond Slider, Yellow Mud Turtle, Eastern Mud Turtle, Eastern Musk Turtle, Smooth Softshell, Spiny Softshell"
+    elif state == "Indiana ":
+        return "Spotted Turtles, Red-eared Sliders, Stinkpot Turtles. ...Eastern Mud Turtles. ...Map Turtles. ...Snapping Turtles. ...Softshell Turtles of Indiana. ...Box Turtles of Indiana."
+    elif state == "Iowa":
+        return "Snapping Turtle  (Chelydra serpentina) Painted Turtle  (Chrysemys picta) Blanding’s Turtle  (Emydoidea blandingii) Wood Turtle  (Clemmys insculpta) Northern Map Turtle (Graptemys geographica) Ouachita Map Turtle (Graptemys ouachitensis)False Map Turtle (Graptemys pseudogeographica)Ornate Box Turtle  (Terrapene ornata)Red-eared Slider (Trachemys scripta)Yellow Mud Turtle  (Kinosternon flavescens)Common Musk Turtle  (Sternotherus odoratus)Smooth Softshell Turtle  (Apalone mutica)Spiny Softshell Turtle (Apalone spinifera)"
+    elif state == "Kansas":
+        return "Alligator snapping turtleCommon snapping turtleCommon musk turtle (stinkpot)Yellow mud turtlePainted turtleCommon map turtleOuachita map turtleFalse map turtleRiver cooterornate box turtleeastern box turtlepond slider turtleSpiny softshellSmooth softshell"
+    elif state == 'Kentucky':
+        return "Smooth Softshell, Spiny Softshell, Common Snapping Turtle, Alligator Snapping Turtle, Eastern Mud Turtle, Common Musk Turtle, Southern/Midland Painted Turtle, Common Map Turtle, Ouachita Map Turtle, Mississippi Map Turtle, False Map Turtle, Eastern River Cooter, Red-eared Slider , Eastern Box Turtle"
+    elif state == "Louisiana":
+        return "Caretta caretta - Loggerhead Sea Turtle Chelonia mydas - Green Sea TurtleEretmochelys imbricata - Hawksbill Sea TurtleLepidochelys kempii - Kemp's Ridley Sea TurtleFamily ChelydridaeChelydra serpentina - Snapping TurtleMacrochelys temminckii - Alligator Snapping TurtleFamily DermochelyidaeDermochelys coriacea - Leatherback Sea TurtleFamily KinosternidaeKinosternon subrubrum - Eastern Mud TurtleSternotherus carinatus - Razor-backed Musk TurtleSternotherus minor - Loggerhead Musk TurtleSternotherus odoratus - Eastern Musk TurtleFamily EmydidaeChrysemys dorsalis - Southern Painted TurtleDeirochelys reticularia - Chicken TurtleGraptemys oculifera - Ringed Map TurtleGraptemys ouachitensis - Ouachita Map TurtleGraptemys pearlensis - Pearl River Map TurtleGraptemys pseudogeographica - False Map TurtleGraptemys sabinensis - Sabine Map TurtleMalaclemys terrapin - Diamond-backed TerrapinPseudemys concinna - River CooterTerrapene carolina - Eastern Box TurtleTerrapene ornata - Ornate Box TurtleTrachemys scripta - Pond SliderFamily TestudinidaeGopherus polyphemus - Gopher TortoiseFamily TrionychidaeApalone ferox - Florida Softshell [Introduced]Apalone mutica - Smooth SoftshellApalone spinifera - Spiny Softshell"
+    elif state == 'Maine':
+        return "Eastern Painted, Midland Painted, Northern Snapping, Musk Turtles Spotted Turtle, Wood Turtle, Eastern Box Turtle, Blanding Turtle" 
+    elif state == "Maryland":
+        return "Eastern Musk Turtle Sternotherus odoratus Eastern Mud Turtle Kinosternon subrubrum subrubrum Eastern Box Turtle Terrapene carolina carolina Spotted Turtle Clemmys guttata Bog Turtle Glyptemys muhlenbergii ThreatenedF,S Wood Turtle Glyptemys insculpta Northern Diamond-backed Terrapin Malaclemys terrapin terrapin Northern Map Turtle Graptemys geographica EndangeredS Midland Painted Turtle Chrysemys picta marginata Eastern Painted Turtle Chrysemys picta picta Red-eared Slider Trachemys scripta elegans Introduced Northern Red-bellied Cooter Pseudemys rubriventris Eastern Snapping Turtle Chelydra serpentina serpentina Loggerhead Sea Turtle Caretta caretta ThreatenedF,S Kemp’s Ridley Sea Turtle Lepidochelys kempii EndangeredF,S Green Sea Turtle Chelonia mydas ThreatenedF,S Leatherback Sea Turtle Dermochelys coriacea EndangeredF,S Atlantic Hawksbill Sea Turtle Eretmochelys imbricata imbricata EndangeredF,S Eastern Spiny Softshell Apalone spinifera spinifera"
+    elif state == 'Massachusetts':
+        return "Turtle Species in Massachusetts. Blanding's Turtle (Emydoidea blandingii) Bog Turtle (Glyptemys muhlenbergii) Diamond-backed Terrapin (Malaclemys terrapin) Eastern Box Turtle (Terrapene carolina) Northern Red-bellied Cooter (Pseudemys rubirentris) Wood turtle (Clemmys insculpta)"
+    elif state == "Michigan":
+        return "Blanding’s Turtle, Common Map Turtle, Common Musk Turtle, Eastern Box Turtle, Midland Painted Turtle, Red-eared Slider, Eastern Spiny Softshell, Spotted Turtle, Common Snapping Turtle, Wood Turtle"
+    elif state == 'Minnesota':
+        return "Blanding's turtle, False map turtle, Northern map turtle, Ouachita map turtle, Painted turtle, Smooth softshell turtle, Snapping turtle, Spiny softshell turtle, Wood turtle"
+    elif state == "Mississippi":
+        return ""
+#Missouri 
+#Montana 
+#Nebraska 
+#Nevada 
+#New Hampshire 
+#New Jersey 
+#New Mexico 
+#New York 
+#North Carolina 
+#North Dakota 
+#Ohio 
+
+#Oklahoma 
+#Oregon 
+#Pennsylvania 
+#Rhode Island 
+#South Carolina 
+#South Dakota 
+#Tennessee 
+#Texas 
+#Utah 
+#Vermont 
+#Virginia 
+#Washington 
+#West Virginia 
+#Wisconsin 
+#Wyoming
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,25 @@ from flask import render_template, request
 from app.models import model, formopener
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return "hello world!"
+    return render_template('index.html')
+
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    if request.method == 'GET':
+        return "N/A"
+    else:
+        userdata = formopener.dict_from(request.form)
+        print(userdata)
+        state = userdata["state"]
+        turtle = model.FindTurtle(state)
+        return render_template("results.html", state = state, turtle = turtle)
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
+    
+@app.route('/Cite', methods=['GET', 'POST'])
+def Cite():
+    return render_template('Cite.html')
